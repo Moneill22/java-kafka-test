c9a50ab9b2f947e2b6f120ff87a3abc8d2b94599
Moneill22 <moneill@umass.edu>
2022-07-27 10:28:00 -0400

    Kafka tutorial with Java

---
+++ kafka-app/src/main/java/SampleProducer.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+public class SampleProducer {
+	
+	public SampleProducer() {
+		Properties properties = new Properties();
+		
+		properties.put("bootstrap.servers","localhost:9092");
+		
+		properties.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
+		properties.put("value.serializer","org.apache.kafka.common.serialization.StringSerializer");
+
+		ProducerRecord producerRecord = new ProducerRecord("channel", "Name", "Leah");
+
+		KafkaProducer kafkaProducer = new KafkaProducer(properties);
+		kafkaProducer.send(producerRecord);
+		kafkaProducer.close();
+	}
+	
+	
+}
